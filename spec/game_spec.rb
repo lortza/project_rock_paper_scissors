9e834d85c88cf1b77f5c664b50065d312597f48d
anne <richardson.ae@gmail.com>
2017-10-02 13:31:38 -0500

    Add specs for score keeping

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -40,11 +40,22 @@ describe Game do
   end #play_two_player
 
   describe '#computer_makes_choice' do
-    it 'returns a randomly selected weapon'
+    let(:acceptable_responses) {["1", "2", "3"]}
+    it 'returns a randomly selected weapon' do
+      computer_choice = game.send(:computer_makes_choice,acceptable_responses)
+      expect(acceptable_responses).to include(computer_choice)
+    end
   end
 
   describe '#announce_round' do
-    it 'increments and returns the round number'
+    before do
+      allow_any_instance_of(IO).to receive(:puts)
+    end
+
+    it 'increments and returns the round number' do
+      game.round_number = 1
+      expect(game.send(:announce_round)).to eq(2)
+    end
   end
 
   describe '#request_player_choice' do
@@ -53,48 +64,124 @@ describe Game do
 
   describe '#determine_round_winner' do
     context 'when players 1 & 2 have the same weapon' do
-      it "the round is a 'tie'"
+      it "the round is a 'tie'" do
+        expect(game.send(:determine_round_winner, 1, 1)).to eq('tie')
+      end
     end
-    context "when player 1 is 'r' and player 2 is 'p" do
-      it 'player 2 wins'
+    context "when player 1 is 'r' and player 2 is 'p'" do
+      it 'player 2 wins' do
+        expect(game.send(:determine_round_winner, 'r', 'p')).to eq('player2')
+      end
     end
-    context "when player 1 is 'p' and player 2 is 's" do
-      it 'player 2 wins'
+    context "when player 1 is 'p' and player 2 is 's'" do
+      it 'player 2 wins' do
+        expect(game.send(:determine_round_winner, 'p', 's')).to eq('player2')
+      end
     end
-    context "when player 1 is 's' and player 2 is 'r" do
-      it 'player 2 wins'
+    context "when player 1 is 's' and player 2 is 'r'" do
+      it 'player 2 wins' do
+        expect(game.send(:determine_round_winner, 's', 'r')).to eq('player2')
+      end
     end
-    it 'i think this spec can get really redundant...'
+    context "when player 2 is 'p' and player 2 is 'r'" do
+      it 'player 1 wins' do
+        expect(game.send(:determine_round_winner, 'p', 'r')).to eq('player1')
+      end
+    end
+    context "when player 2 is 's' and player 2 is 'p'" do
+      it 'player 1 wins' do
+        expect(game.send(:determine_round_winner, 's', 'p')).to eq('player1')
+      end
+    end
+    context "when player 2 is 'r' and player 2 is 's'" do
+      it 'player 1 wins' do
+        expect(game.send(:determine_round_winner, 'r', 's')).to eq('player1')
+      end
+    end
   end
 
   describe '#announce_round_winner' do
+    it 'puts the round winner in the console'
   end
 
   describe '#determine_game_winner' do
     context "when player 1 and player 2 have the same score" do
-      it 'the game is a tie'
+      it 'the game is a tie' do
+        game.player1_score = 1
+        game.player2_score = 1
+        expect(game.send(:determine_game_winner)).to eq('tie')
+      end
     end
     context "when player 1's score is higher than player 2's score" do
-      it 'player 1 wins the game'
+      it 'player 1 wins the game' do
+        game.player1_score = 2
+        game.player2_score = 1
+        expect(game.send(:determine_game_winner)).to eq('player1')
+      end
     end
     context "when player 1's score is lower than player 2's score" do
-      it 'player 2 wins the game'
+      it 'player 2 wins the game' do
+        game.player1_score = 1
+        game.player2_score = 2
+        expect(game.send(:determine_game_winner)).to eq('player2')
+      end
     end
   end
 
   describe '#award_points' do
     context 'when the round is a tie' do
-      it 'awards points to neither player'
+      it 'does not awards points to player 1' do
+        game.round_winner = 'tie'
+        player1_before = game.player1_score
+        game.send(:award_points)
+        player1_after = game.player1_score
+        expect(player1_before).to eq(player1_after)
+      end
+
+      it 'does not awards points to player 2' do
+        game.round_winner = 'tie'
+        player2_before = game.player2_score
+        game.send(:award_points)
+        player2_after = game.player2_score
+        expect(player2_before).to eq(player2_after)
+      end
     end
+
     context 'when player 1 wins the round' do
-      it "one point is added to player 1's score"
+      it "one point is added to player 1's score" do
+        game.round_winner = 'player1'
+        game.player1_score = 0
+        game.send(:award_points)
+        expect(game.player1_score).to eq(1)
+      end
+
+      it 'does not awards points to player 2' do
+        game.round_winner = 'player1'
+        game.player2_score = 0
+        game.send(:award_points)
+        expect(game.player2_score).to eq(0)
+      end
     end
+
     context 'when player 2 wins the round' do
-      it "one point is added to player 2's score"
+      it "one point is added to player 2's score" do
+        game.round_winner = 'player2'
+        game.player2_score = 0
+        game.send(:award_points)
+        expect(game.player2_score).to eq(1)
+      end
+
+      it 'does not awards points to player 1' do
+        game.round_winner = 'player2'
+        game.player1_score = 0
+        game.send(:award_points)
+        expect(game.player1_score).to eq(0)
+      end
     end
   end
 
   describe '#announce_game_winner' do
+    it 'puts the game winner in the console'
   end
 
   describe '#play' do
