bc903ef7e1db6c3a3f4e88da9aca9cc60e5c7b64
anne <richardson.ae@gmail.com>
2017-10-03 20:37:06 -0500

    Refactor for clarity of code blocks

--- spec/round_winner_spec.rb
+++ spec/round_winner_spec.rb
@@ -2,10 +2,12 @@ require 'winner'
 
 describe RoundWinner do
   describe '#call' do
-    let(:weapons) {}
+    let(:weapons) { }
     let(:round_winner) { RoundWinner.new(weapons) }
 
-    subject { round_winner.call(player1_weapon, player2_weapon) }
+    subject do
+     round_winner.call(player1_weapon, player2_weapon)
+    end
 
     describe 'when the weapons are equal' do
       let(:player1_weapon) { 'a' }
@@ -15,12 +17,9 @@ describe RoundWinner do
     end
 
     describe 'when player 1s weapon is greater' do
-      let(:weapons) {
-        {
-          ['a', '1'] => 'player2'
-        }
-      }
-
+      let(:weapons) do
+        { ['a', '1'] => 'player2' }
+      end
       let(:player1_weapon) { 'b' }
       let(:player2_weapon) { '1' }
 
@@ -28,11 +27,9 @@ describe RoundWinner do
     end
 
     describe 'when player 2s weapon is greater' do
-      let(:weapons) {
-        {
-          ['a', '1'] => 'player2'
-        }
-      }
+      let(:weapons) do
+        { ['a', '1'] => 'player2' }
+      end
       let(:player1_weapon) { 'a' }
       let(:player2_weapon) { '1' }
 
